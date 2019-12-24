ccdd42b229c4979c043139f2eaee18c33a928835
patszy <patszy97@interia.pl>
2019-12-24 10:22:04 +0100

    Problem to solve

--- main.js
+++ main.js
@@ -1,58 +1,56 @@
+class Employee{
+    constructor(index, workTime){
+        this.index = index;
+        this.workTime = workTime;
+    }
+}
+
 function calcSalary(){
-    let getEmployee, getTime, getPrice, getSalary, sumSalary;
+    let getEmployee, time, price, getSalary, sumSalary;
 
     for(let i=1; true; i++){
         getEmployee = document.querySelector('#pracownik'+i);
+
         if(getEmployee == null) break;
-        console.log(getEmployee);
+
         time = getEmployee.getElementsByTagName('input')[0].value;
         price = getEmployee.getElementsByTagName('input')[1].value;
         getSalary = getEmployee.getElementsByClassName('wyplata')[0];
-        console.log(time);
-        console.log(price);
-        console.log(getSalary);
-        if(time<=160){
-            sumSalary = time*price;
-            if(time<100) getEmployee.style.backgroundColor = 'maroon';
-        }
+
+        if(time<=160) sumSalary = time*price;
         else sumSalary = 160*price+(time-160)*price*2;
-        console.log(sumSalary);
+
         getSalary.innerHTML = sumSalary;
     }
 }
 
-function bestEmployee(){
-    let getEmployee, getTime, bestEmployee=[0,0,0];
+function rateEmployee(){
+    let getEmployee, getTime, getBestEmployee, tabEmployee=[];
+
     for(let i=1; true; i++){
         getEmployee = document.querySelector('#pracownik'+i);
         if(getEmployee == null) break;
-        console.log(getEmployee);
         getTime = parseInt(getEmployee.getElementsByTagName('input')[0].value);
-        console.log(getTime);
-
-        if(getTime>bestEmployee[0]){
-            if(getTime>bestEmployee[1]){
-                if(getTime>bestEmployee[2]){
-                    bestEmployee[0]=bestEmployee[1];
-                    console.log(bestEmployee[0]);
-                    bestEmployee[1]=bestEmployee[2];
-                    console.log(bestEmployee[1]);
-                    bestEmployee[2]=getTime;
-                    console.log(bestEmployee[2]);
-                } else{
-                    bestEmployee[0]=bestEmployee[1];
-                    bestEmployee[1]=getTime;
-                }
-            } else bestEmployee[0]=getTime;
-        }
-        bestEmployee.forEach(function(element){
-            console.log(element);
-        });
+        let addEmployee = new Employee(i, getTime);
+        tabEmployee[i-1]=addEmployee;
     }
+
+    /* Best 3 employees */
+    getBestEmployee = document.getElementById('najlepsi-pracownicy');
+    tabEmployee.sort((empl1, empl2) => (empl1.workTime > empl2.workTime) ? -1 : 1);
+    for(i=0; i<3; i++){
+        getEmployee = document.querySelector('#pracownik'+tabEmployee[i].index);
+        getBestEmployee.appendChild(getEmployee);
+    }
+    /* How to repair best 3 employees mistake: create object.name, get name from employee, add to object, view it in html. */
+
+    /* Worst employee/s */
+    getEmployee = document.querySelector('#pracownik'+tabEmployee[tabEmployee.length-1].index);
+    getEmployee.style.backgroundColor = 'maroon';
 }
 
-/* calcSalary(); */
-/* bestEmployee(); */
+
 document.getElementsByTagName('button')[0].addEventListener('click', function(){
     calcSalary();
-})
+    rateEmployee();
+});
